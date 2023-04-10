a4f8cd0458069e78b43b632d77da4551254f9be0
mattmc3 <mattmc3@gmail.com>
2023-04-10 18:57:09 -0400

    Update project template

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.zsh
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.zsh
@@ -1,14 +1,12 @@
+#
+# {{cookiecutter.project_name}} - Description here...
+#
+
 # add your plugin code here... you can delete everything
 # in this file and write your thing
 echo "{{cookiecutter.project_name}}"
 
 # if this project has a functions dir, this block shows how to autoload
 # it is safe to remove this if you don't want/need it
-if [[ -d ${0:A:h}/functions ]]; then
-  fpath+=${0:A:h}/functions
-  for _fn in ${0:A:h}/functions/*(.N); do
-    # unfunction "${_fn:t}" &>/dev/null
-    autoload -Uz $_fn
-  done
-  unset _fn
-fi
+fpath=(${0:A:h}/functions $fpath)
+autoload -z $fpath[1]/*(.:t)
