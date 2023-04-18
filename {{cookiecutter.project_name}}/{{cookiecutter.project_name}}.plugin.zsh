2b7c5871a4f5400ab20fb3c964185bcb09781519
mattmc3 <mattmc3@gmail.com>
2023-04-17 22:33:11 -0400

    Remove second .zsh file

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.plugin.zsh
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}.plugin.zsh
@@ -1,2 +1,12 @@
-0=${(%):-%N}
-source ${0:A:h}/{{cookiecutter.project_name}}.zsh
+#
+# {{cookiecutter.project_name}} - Description here...
+#
+
+# add your plugin code here... you can delete everything
+# in this file and write your thing
+echo "{{cookiecutter.project_name}}"
+
+# if this project has a functions dir, this block shows how to autoload
+# it is safe to remove this if you don't want/need it
+fpath=(${0:A:h}/functions $fpath)
+autoload -z $fpath[1]/*(.:t)
